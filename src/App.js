89cd23b03dfa30b683c766bb9714196fd109dbe1
ricmonmol <jacksonfkk@gmail.com>
2023-02-25 11:54:08 -0300

    ARS + styles updates

--- src/App.js
+++ src/App.js
@@ -3,34 +3,36 @@ import './App.css';
 import axios from 'axios'
 import Coin from './Coin';
 
-
 function App() {
-  const [coins,setCoins] = useState([])
-  const [search,setSearch] = useState('')
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+  
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then(res=>{
+    {/*axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')*/}
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=ARS&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then((res) => {
        setCoins(res.data)
        console.log(res.data)
-    }).catch(error=>console.log(error))
+    }).catch((error) => console.log(error))
   }, [])
-  const handleChange = e =>{
+  
+  const handleChange = e => {
     setSearch(e.target.value)
   }
-  const filteredCoins = coins.filter(coin=>
+  
+  const filteredCoins = coins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase())
     )
+  
   return (
     <div className="coin-app">
       <div className="coin-search">
         {/* <h1 className="coin-text">Search your desired coin</h1> */}
         <form action="">
           <input type="text" className="coin-input" placeholder="Provide the coin name" onChange={handleChange}/>
-
         </form>
-
       </div>
-      {filteredCoins.map(coin=>{
+      {filteredCoins.map((coin) => {
         return(
           <Coin 
           key={coin.id} 
@@ -40,12 +42,9 @@ function App() {
           marketcap={coin.market_cap}
           price={coin.current_price}
           pricechange={coin.price_change_percentage_24h}
-//           volume={coin.total_volume}
           />
         );
       })}
-
-
     </div>
   );
 }

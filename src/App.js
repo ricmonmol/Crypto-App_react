c1a6ab01bbb9b4a6eebe032b0eb246c8b0d8ae22
Ricardo <jacksonfkk@gmail.com>
2023-03-25 12:25:53 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,95 +1,130 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios'
-import Coin from './Coin';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Coin from "./Coin";
 
 function App() {
-  const [coins, setCoins] = useState([])
-  const [search, setSearch] = useState(' ')
-  
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState(" ");
+
   useEffect(() => {
-    {/*axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')*/}
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=ARS&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then((res) => {
-       setCoins(res.data)
-       console.log(res.data)
-    }).catch((error) => console.log(error))
-  }, [])
-
-  const filteredCoins = coins.filter(e => {
-    if(search === ' ') return coins
-    else { 
-      return e.name.toLowerCase().includes(search)
+    // eslint-disable-next-line
+    {
+      /*axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')*/
     }
-  })
-
-  function handleSort(e){
-    if(e.target.value === 'orderCurrentPrice'){
-        const sortedCoins = [...coins].sort((a,b) => {
-          if(a.current_price > b.current_price ){
-          return -1
-          } else {
-          return 1
-          }
-        })
-      setCoins(sortedCoins)
-    }
-    if(e.target.value === 'orderPriceChange'){ 
-      const sortedCoins = [...coins].sort((a,b) => {
-        if(a.price_change_percentage_24h > b.price_change_percentage_24h){
-          return -1
-        } else {
-          return 1 
-        }
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ARS&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
       })
-      setCoins(sortedCoins)
+      .catch((error) => console.log(error));
+  }, []);
+
+  const filteredCoins = coins.filter((e) => {
+    if (search === " ") return coins;
+    else {
+      return e.name.toLowerCase().includes(search);
+    }
+  });
+
+  function handleSort(e) {
+    if (e.target.value === "orderName") {
+      const sortedCoins = [...coins].sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+      setCoins(sortedCoins);
+    }
+    if (e.target.value === "orderCurrentPrice") {
+      const sortedCoins = [...coins].sort((a, b) => {
+        if (a.current_price > b.current_price) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      setCoins(sortedCoins);
+    }
+    if (e.target.value === "orderPriceChange") {
+      const sortedCoins = [...coins].sort((a, b) => {
+        if (a.price_change_percentage_24h > b.price_change_percentage_24h) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      setCoins(sortedCoins);
     }
 
-    if(e.target.value ==='orderMktCap'){
-      const sortedCoins = [...coins].sort((a,b) => {
-        if(a.market_cap > b.market_cap) {
-          return -1
+    if (e.target.value === "orderMktCap") {
+      const sortedCoins = [...coins].sort((a, b) => {
+        if (a.market_cap > b.market_cap) {
+          return -1;
         } else {
-          return 1
+          return 1;
         }
-      })
-      setCoins(sortedCoins)
+      });
+      setCoins(sortedCoins);
     }
   }
 
-  function handlerChange(e){
-    setSearch(e.target.value.toLowerCase())
+  function handlerChange(e) {
+    setSearch(e.target.value.toLowerCase());
   }
 
   return (
-    <div className='coinApp'>
-      <h1 id='title' className='h1Title'>CRYPTOWATCHER</h1>
-      <form className='formSearch'>
+    <div className="coinApp">
+      <h1 id="title" className="h1Title">
+        CRYPTOWATCHER
+      </h1>
+      <form className="formSearch">
         <input
-          placeholder='Buscar crypto'
-          name='search'
+          placeholder="Buscar crypto"
+          name="search"
           onChange={handlerChange}
-          className='inputSearch'
+          className="inputSearch"
         />
       </form>
-      <div className='orderBtnList'>
-        <p>Ordenar por:</p><select onClick={handleSort}>
-          <option value='' disabled selected>Elija una opción</option>
-          <option value='orderCurrentPrice'>Por Precio</option>
-          <option value='orderPriceChange'>Por Variación de Precio</option>
-          <option value='orderMktCap'>Por Capitalización de Mercado</option>
-        </select>
-      </div>  
+      <p className="pOrder">Ordenar por: </p>
+      <div className="btnTitle">
+        <button className="btnOrder" onClick={handleSort} value="orderName">
+          Crypto
+        </button>
+        <button
+          className="btnOrder"
+          onClick={handleSort}
+          value="orderCurrentPrice"
+        >
+          Precio
+        </button>
+        <button
+          className="btnOrder"
+          onClick={handleSort}
+          value="orderPriceChange"
+        >
+          Variación
+        </button>
+        <button className="btnOrder" onClick={handleSort} value="orderMktCap">
+          Capitalización
+        </button>
+      </div>
+
       {filteredCoins.map((coin) => {
-        return(
-          <Coin 
-          key={coin.id} 
-          name={coin.name} 
-          image={coin.image} 
-          symbol={coin.symbol}
-          marketcap={coin.market_cap}
-          price={coin.current_price}
-          pricechange={coin.price_change_percentage_24h}
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            pricechange={coin.price_change_percentage_24h}
           />
         );
       })}
--- src/Coin.js
+++ src/Coin.js
@@ -1,32 +1,28 @@
-import React from 'react'
-import './Coin.css'
+import React from "react";
+import "./Coin.css";
 
-const Coin = ({image, name, price, volume, pricechange, marketcap}) => {
-    return (
-        <div className='coinContainer'>
-            <div className={pricechange > 0 ? 'coinRowGreen' : 'coinRowRed'}>
-                <div className='coin'>
-                    <img src={image} alt="crypto" />
-                    <h1>{name}</h1>
-                    {/*<p className="coin-symbol"></p>*/}
-                </div>
-                <div className='coinData'>
-                    <p className='coinPrice'>ARS$: {price}</p>
-                    {/* <p className="coin-volume">Rs.{volume.toLocaleString()}</p>*/}
-                    {pricechange < 0 ? (
-                        <p className='coinPercent red'>{pricechange.toFixed(2)}% &#9660;</p>
-                    ):(
-                        <p className='coinPercent green'>{pricechange.toFixed(2)}% &#9650;</p>
-                    )
-                }
-                    <p className='coinMarketcap'>
-                    Cap. Mercado ARS$: {marketcap.toLocaleString()}
-                </p>
-                </div>
-            </div>
-            
+const Coin = ({ image, name, price, pricechange, marketcap }) => {
+  return (
+    <div className="coinContainer">
+      <div className={pricechange > 0 ? "coinRowGreen" : "coinRowRed"}>
+        <div className="coin">
+          <img src={image} alt="crypto" />
+          <h1>{name}</h1>
         </div>
-    )
-}
+        <div className="coinData">
+          <p className="coinPrice">ARS$: {price.toLocaleString()}</p>
+          {pricechange < 0 ? (
+            <p className="coinPercent red">{pricechange.toFixed(2)}% &#9660;</p>
+          ) : (
+            <p className="coinPercent green">
+              {pricechange.toFixed(2)}% &#9650;
+            </p>
+          )}
+          <p className="coinMarketcap">ARS$: {marketcap.toLocaleString()}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Coin
+export default Coin;
